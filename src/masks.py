09933bde9ee2de6ef4d0bed7278251f5469b4b8e
Petr Batakshov <petrobatakshov@yandex.ru>
2024-11-09 11:51:34 +0300

    correcting homework errors

--- src/masks.py
+++ src/masks.py
@@ -1,13 +1,10 @@
-from typing import Union
-
-
-def get_mask_card_number(card_num: Union[str]) -> Union[str]:
+def get_mask_card_number(card_num: str) -> str:
     """Функция показывает первые 6 цифр и последние 4 цифры в блоке по 4 цифры,
     а остальные цифры заменяет на символ'*'"""
     return f"{card_num[0:4]} {card_num[4:6]}** **** {card_num[-4:]}"
 
 
-def get_mask_account(card_num: list[str]) -> str:
+def get_mask_account(card_num: str) -> str:
     """ "Функция заменяет все цифры символом '*' и возвращает последние 4 цифры"""
     return f"**{card_num[-4:]}"
 
