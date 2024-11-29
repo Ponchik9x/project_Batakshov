53f294bdb544ccb38d0a1d5e3c8458399ee3c7a5
Petr Batakshov <petrobatakshov@yandex.ru>
2024-11-29 18:01:05 +0300

    fixed masks

--- src/masks.py
+++ src/masks.py
@@ -1,9 +1,13 @@
 def get_mask_card_number(card_num: str) -> str:
     """Функция показывает первые 6 цифр и последние 4 цифры в блоке по 4 цифры,
     а остальные цифры заменяет на символ'*'"""
+    if len(card_num) > 16 or len(card_num) < 16:
+        raise ValueError("неверный номер карты")
     return f"{card_num[0:4]} {card_num[4:6]}** **** {card_num[-4:]}"
 
 
 def get_mask_account(card_num: str) -> str:
     """ "Функция заменяет все цифры символом '*' и возвращает последние 4 цифры"""
-    return f" **{card_num[-4:]}"
+    if len(card_num) > 20 or len(card_num) < 20:
+        raise ValueError("неверный номер карты")
+    return f"**{card_num[-4:]}"
