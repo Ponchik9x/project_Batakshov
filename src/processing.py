e931f8c8eec549a3d1fcaf16cdbd727e8b59cf11
Petr Batakshov <petrobatakshov@yandex.ru>
2024-12-01 23:00:39 +0300

    add tests for processing and fixed processing.py

--- src/processing.py
+++ src/processing.py
@@ -1,10 +1,10 @@
 def filter_by_state(state_dict: list[dict[str, object]], state: str = "EXECUTED") -> list[dict[str, object]]:
     """Функция возвращает список словарей по значению ключа "state" = EXECUTED"""
-    new_dict = []
+    filter_list = []
     for dict_ in state_dict:
         if dict_["state"] == state:
-            new_dict.append(dict_)
-    return new_dict
+            filter_list.append(dict_)
+    return filter_list
 
 
 def sort_by_date(users_dict: list, reverse_list_dict: bool = True) -> list:
