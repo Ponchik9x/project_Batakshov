2bc371edcd31a8365b1f472c079b385920904c29
Petr <petrobatakshov@yandex.ru>
2024-12-25 11:47:39 +0300

    fix lint main.py

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from datetime import datetime
 
-from examples import (account_card_number, account_card_number_invalid, account_number_score,
-                      account_number_score_invalid, my_dict, transactions)
+from examples import (
+    account_card_number,
+    account_card_number_invalid,
+    account_number_score,
+    account_number_score_invalid,
+    my_dict,
+    transactions,
+)
 from src.generators import card_number_generator, filter_by_currency, transaction_descriptions
 from src.masks import get_mask_account, get_mask_card_number
 from src.processing import filter_by_state, sort_by_date
