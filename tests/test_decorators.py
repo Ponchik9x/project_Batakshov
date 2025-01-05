5fec00f2cf63305037f77e8157792be6a423695b
Petr <petrobatakshov@yandex.ru>
2025-01-05 11:39:16 +0300

    fix decorators and test_decorators

--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -2,6 +2,7 @@ from src.decorators import log
 
 
 def test_log():
+    """ тест на проверку правильности работы декоратора без добавления в модуль для логов """
     @log()
     def add(x, y):
         return x + y
@@ -10,6 +11,7 @@ def test_log():
 
 
 def test_log_filename():
+    """ тест на проверку правильности работы декоратора добавлением в определенный модуль для логов """
     @log("log_file")
     def add(x, y):
         return x + y
